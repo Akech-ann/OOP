08f90732283a1ff9b07ef8f1036941d19dd4085f
Akech-ann <akechannatong@gmail.com>
2023-03-02 08:25:56 +0300

    made changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,8 @@ fun main() {
     var ann = Human("Akech",20,54.5)
     ann.eat(3)
     ann.speak("I am a perfectionist")
-    ann.birthday(1)
-
+    ann.birthday()
+    println(ann.age)
     var me = anny("Akechi","Montana","akechann@gmail.com","073849484",64546)
     println(me.email)
     println(me.lastname)
@@ -18,9 +18,10 @@ class Human(var name:String,var age:Int,var weight:Double){
 
 
     }
-    fun birthday(plusone:Int){
+    fun birthday(){
         age += 1
-        println(age)
+
+
     }
 
 
